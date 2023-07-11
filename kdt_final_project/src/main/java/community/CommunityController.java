15d5ca148f8b7ec6d435758d5f77449206d3cfce
ANKYUNGMIN <ANKYUNGMIN@DESKTOP-5TLBDV4>
2023-07-11 09:22:48 +0900

    ckeditor파일 추가, 커뮤니티 기능은 만드는중

--- kdt_final_project/src/main/java/community/CommunityController.java
+++ kdt_final_project/src/main/java/community/CommunityController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CommunityController {
+	
 	@RequestMapping("/community")
 	public String test() {
 		return "community";

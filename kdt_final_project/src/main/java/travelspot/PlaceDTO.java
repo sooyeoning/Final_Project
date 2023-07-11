1060fc1dfec269999e0110165a515b6da7629056
sooyeoning <135004060+sooyeoning@users.noreply.github.com>
2023-07-11 09:26:18 +0900

    Update PlaceDTO.java

--- kdt_final_project/src/main/java/travelspot/PlaceDTO.java
+++ kdt_final_project/src/main/java/travelspot/PlaceDTO.java
@@ -1,7 +1,6 @@
 package travelspot;
 
 public class PlaceDTO {
-	int seq;
 	int contentId;
 	String title;
 	int areaCode;
@@ -11,16 +10,77 @@ public class PlaceDTO {
 	double mapx;
 	double mapy;
 	String contents;
-	int viewcount;
+	String theme;
 	String writingtime;
-	
 	int likecnt;
 	int viewcnt;
+	String homepage;
+
+	public String getHomepage() {
+		return homepage;
+	}
+
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+
+	public PlaceDTO() { }//기본생성자 
 	
-	//모든 필드 포함한 생성자
-	public PlaceDTO(int seq, int contentId, String title, int areaCode, String image1, String image2, String address,
-			double mapx, double mapy, String contents, int viewcount, String writingtime, int likecnt, int viewcnt) {
-		this.seq = seq;
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getTheme() {
+		return theme;
+	}
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	
+	public String getWritingtime() {
+		return writingtime;
+	}
+
+	public void setWritingtime(String writingtime) {
+		this.writingtime = writingtime;
+	}
+
+	public int getLikecnt() {
+		return likecnt;
+	}
+
+	public void setLikecnt(int likecnt) {
+		this.likecnt = likecnt;
+	}
+
+	public int getViewcnt() {
+		return viewcnt;
+	}
+
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+
+	public PlaceDTO(int contentId, String title, int areaCode, String image1, String address, double mapx, double mapy, String contents, String homepage) {
+		this.contentId = contentId;
+		this.title = title;
+		this.areaCode = areaCode;
+		this.image1 = image1;
+		this.address = address;
+		this.mapx = mapx;
+		this.mapy = mapy;
+		this.contents = contents;
+		this.homepage = homepage;
+	};
+
+	public PlaceDTO(int contentId, String title, int areaCode, String image1, String image2, String address,
+			double mapx, double mapy, String contents, String theme, String writingtime, int likecnt, int viewcnt,
+			String homepage) {
 		this.contentId = contentId;
 		this.title = title;
 		this.areaCode = areaCode;
@@ -30,43 +90,68 @@ public class PlaceDTO {
 		this.mapx = mapx;
 		this.mapy = mapy;
 		this.contents = contents;
-		this.viewcount = viewcount;
+		this.theme = theme;
+		this.writingtime = writingtime;
+		this.likecnt = likecnt;
+		this.viewcnt = viewcnt;
+		this.homepage = homepage;
+	}
+
+	public PlaceDTO(int contentId, String title, int areaCode, String image1, String image2, String address,
+			double mapx, double mapy, String contents,String theme, String writingtime, int likecnt, int viewcnt
+			) { // 테마o
+		super();
+		this.contentId = contentId;
+		this.title = title;
+		this.areaCode = areaCode;
+		this.image1 = image1;
+		this.image2 = image2;
+		this.address = address;
+		this.mapx = mapx;
+		this.mapy = mapy;
+		this.contents = contents;
+		this.writingtime = writingtime;
+		this.likecnt = likecnt;
+		this.viewcnt = viewcnt;
+		this.theme = theme;
+	}
+
+	public PlaceDTO(int contentId, String title, int areaCode, String image1, String image2, String address,
+			double mapx, double mapy, String contents, String writingtime, int likecnt, int viewcnt) { // 테마x																												// x
+		this.contentId = contentId;
+		this.title = title;
+		this.areaCode = areaCode;
+		this.image1 = image1;
+		this.image2 = image2;
+		this.address = address;
+		this.mapx = mapx;
+		this.mapy = mapy;
+		this.contents = contents;
 		this.writingtime = writingtime;
 		this.likecnt = likecnt;
 		this.viewcnt = viewcnt;
 	}
+
+	//테마별 추천코스에서 장소Id, 장소명 추출용 dto
+	public PlaceDTO(int contentId, String title, String theme) {
+		this.contentId = contentId;
+		this.title = title; 
+		this.theme = theme;
+		this.image1 = image1;
+	}; 
 	
-	public PlaceDTO(int seq, int contentId, String title, int areaCode, String image1, String image2, String address,
-			double mapx, double mapy, String contents, int viewcount, String writingtime) {
-		this.seq = seq;
+	public PlaceDTO(int contentId, String title, int areaCode, String image1,String image2, String address, double mapx, double mapy) { 
 		this.contentId = contentId;
-		this.title = title;
-		this.areaCode = areaCode;
-		this.image1 = image1;
-		this.image2 = image2;
-		this.address = address;
+		this.title = title; 
+		this.areaCode = areaCode; 
+		this.image1 = image1; 
+		this.image2 = image2; 
+		this.address = address; 
 		this.mapx = mapx;
 		this.mapy = mapy;
-		this.contents = contents;
-		this.viewcount = viewcount;
-		this.writingtime = writingtime;
 	}
 
-	public PlaceDTO(int contentId, String title, int areaCode, String image1, String image2, String address,
-			double mapx, double mapy) {
-		this.contentId = contentId;
-		this.title = title;
-		this.areaCode = areaCode;
-		this.image1 = image1;
-		this.image2 = image2;
-		this.address = address;
-		this.mapx = mapx;
-		this.mapy = mapy;
-		//this.contents = contents;
-	}
-
-	public PlaceDTO(int contentId, String title, int areaCode, String image1, String address, double mapx,
-			double mapy) {
+	public PlaceDTO(int contentId, String title, int areaCode, String image1, String address, double mapx, double mapy) {
 		this.contentId = contentId;
 		this.title = title;
 		this.areaCode = areaCode;
@@ -99,40 +184,45 @@ public class PlaceDTO {
 	public void setImage2(String image2) {
 		this.image2 = image2;
 	}
-	
+
 	public int getContentId() {
 		return contentId;
 	}
+
 	public void setContentId(int contentId) {
 		this.contentId = contentId;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public double getMapx() {
 		return mapx;
 	}
+
 	public void setMapx(double mapx) {
 		this.mapx = mapx;
 	}
+
 	public double getMapy() {
 		return mapy;
 	}
+
 	public void setMapy(double mapy) {
 		this.mapy = mapy;
 	}
-	/*
-	 * public String getContents() { return contents; } public void
-	 * setContents(String contents) { this.contents = contents; }
-	 */
-	
+
 }

38a96bf72fb287c063da61d990f57be62363a50b
ANKYUNGMIN <ANKYUNGMIN@DESKTOP-5TLBDV4>
2023-07-15 18:39:05 +0900

    수정,삭제,신고 버튼 추가

--- kdt_final_project/src/main/java/community/CommunityController.java
+++ kdt_final_project/src/main/java/community/CommunityController.java
@@ -23,66 +23,77 @@ public class CommunityController {
 	@Autowired
     private BoardService boardService;
 	
+	// 커뮤니티 페이지 매핑
 	@RequestMapping("/community")
 	public String community(Model model) {
-	    List<BoardDTO> boardList = boardService.getAllBoards(); // Board 목록을 가져오는 메서드 호출
+	    List<BoardDTO> boardList = boardService.getAllBoards();
 	    
-	 // 좋아요 수에 따라 정렬
+	    // 좋아요 수에 따라 정렬
 	    Collections.sort(boardList, Comparator.comparingInt(BoardDTO::getLikecount).reversed());
 
-	    model.addAttribute("boardList", boardList); // boardList를 모델에 추가
+	    model.addAttribute("boardList", boardList);
 	    
-	    List<BoardDTO> top10List = boardService.getTop10Boards(); // Top 10 게시글 목록을 가져오는 메서드 호출
-	    model.addAttribute("top10List", top10List); // top10List를 모델에 추가
+	    List<BoardDTO> top10List = boardService.getTop10Boards();
+	    model.addAttribute("top10List", top10List);
 
 	    return "community";
 	}
-	
+
+	// 새글쓰기 화면 writing.jsp 매핑
 	@RequestMapping("/writing")
 	public String writingForm(HttpServletRequest request, Model model) {
-		UserDTO user = (UserDTO) request.getSession().getAttribute("user");
+	    UserDTO user = (UserDTO) request.getSession().getAttribute("user");
 	    if (user == null) {
-	        // 로그인되지 않은 경우 로그인 페이지로 리다이렉트
 	        return "redirect:/login";
-	    }else if (user != null) {
+	    } else if (user != null) {
 	        model.addAttribute("nickname", user.getNickname());
 	    }
-		return "writing";
+	    return "writing";
 	}
-	
+
+	// 글쓰기 누르면 DB에 저장
 	@PostMapping("/boardwrite")
-    public ModelAndView boardWritePro(@RequestParam("board_title") String board_title,
-                                @RequestParam("place") String place,
-                                //@RequestParam("hashtag") String hashtag,
-                                @RequestParam("writer") String writer,
-                                @RequestParam("title") String title,
-                                @RequestParam("contents") String contents) {
-        BoardDTO board = new BoardDTO();
-        board.setBoard_title(board_title);
-        board.setPlace(place);
-        //board.setHashtag(hashtag);
-        board.setWriter(writer);
-        board.setTitle(title);
-        board.setContents(contents);
+	public ModelAndView boardWritePro(HttpServletRequest request) {
+	    BoardDTO board = new BoardDTO();
+	    board.setBoard_title(request.getParameter("board_title"));
+	    board.setPlace(request.getParameter("place"));
+	    board.setWriter(request.getParameter("writer"));
+	    board.setTitle(request.getParameter("title"));
+	    board.setContents(request.getParameter("contents"));
 
-        boardService.createBoard(board);
+	    boardService.createBoard(board);
 
-        // 게시글 저장 후 어떤 페이지로 이동할지 결정하여 반환
-        //return "redirect:/board/" + board.getId(); // 게시글 상세 페이지로 이동
-        
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("board", board); // 작성한 글을 모델에 추가
-
-        mv.setViewName("board/detail"); // 작성한 글을 보여줄 페이지로 이동
-        return mv;
-	}
-	
-	@GetMapping("/detail")
-	public ModelAndView showBoardDetail(@RequestParam("boardId") int boardId) {
 	    ModelAndView mv = new ModelAndView();
-	    BoardDTO board = boardService.getBoardById(boardId);
 	    mv.addObject("board", board);
 	    mv.setViewName("board/detail");
+	    return mv;
+	}
+
+	// detail.jsp로 이동
+	@GetMapping("/detail")
+	public ModelAndView showBoardDetail(HttpServletRequest request) {
+	    ModelAndView mv = new ModelAndView();
+	    int boardId = Integer.parseInt(request.getParameter("boardId"));
+	    BoardDTO board = boardService.getBoardById(boardId);
+	    mv.addObject("board", board);
+
+	    UserDTO user = (UserDTO) request.getSession().getAttribute("user");
+	    if (user != null) {
+	        mv.addObject("nickname", user.getNickname());
+	    }
+
+	    mv.setViewName("board/detail");
+	    return mv;
+	}
+
+	// update.jsp로 이동
+	@GetMapping("/update")
+	public ModelAndView showBoardUpdateForm(HttpServletRequest request) {
+	    ModelAndView mv = new ModelAndView();
+	    int boardId = Integer.parseInt(request.getParameter("boardId"));
+	    BoardDTO board = boardService.getBoardById(boardId);
+	    mv.addObject("board", board);
+	    mv.setViewName("board/update");
 	    return mv;
 	}
 	

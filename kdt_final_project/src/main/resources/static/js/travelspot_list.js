b6c866be29671a5b00a0a996428b03ff9baa13a3
sooyeoning <135004060+sooyeoning@users.noreply.github.com>
2023-07-18 17:35:44 +0900

    Revert "sooyeon"

--- kdt_final_project/src/main/resources/static/js/travelspot_list.js
+++ kdt_final_project/src/main/resources/static/js/travelspot_list.js
@@ -1,38 +1,37 @@
 $(document).ready(function(){
 	
-	//선택된 지역명(selectedRegion)
+	//선택된 지역명(selectedRegion) 흐림 처리 - 첫번째 말고 다른것도 흐림 처리 필요
 	let urlParams = new URL(location.href).searchParams;
 	let areaCode = urlParams.get('areaCode');
 	
-	//선택된 지역 사진 흐림 처리
-	$(`img[id='${areaCode}']`).css("opacity","0.5");
-	$(`img[id='${areaCode}']`).css("border","2px solid #2463d3");
+	//선택된 지역명 변경 처리 필요
 	
-	//클릭시 페이지 이동
+	//확인용 console.log(areaCode);
+	let selectedRegion= $(".profile").attr("id");
+	if(areaCode === selectedRegion){
+		$(`img[id='${areaCode}']`).css("opacity","0.5");
+		$(`img[id='${areaCode}']`).css("border","2px solid #2463d3");
+	}
+	
+	//지역명 클릭시 화면 이동 및 글자색상 변경
 	$(".placeprofile").click(function(){
+		$(this).css("color","#2463d3"); 
 		//선택된 지역명 전달
 		let contentId = $(this).attr('id');
-		location.href="http://localhost:8099/travelspot/post?contentId="+contentId
-	});
-	
-	$(".placeName").click(function(){ 
-		let contentId = $(this).attr("id");
-		location.href="http://localhost:8099/travelspot/post?contentId="+contentId
-	});
-	
-	$(".placeLocation").click(function(){
-		let contentId = $(this).attr("id");
-		location.href="http://localhost:8099/travelspot/post?contentId="+contentId//이동		
-	});
-	
-	//마우스 올리면 색변경
-	$(".placeName").hover(function(){
+		location.href="http://localhost:8099/travelspot/post?contentId="+contentId//이동
+	})
+	$(".placeName").click(function(){
 		$(this).css("color","#2463d3"); 
-	}, function(){
-		$(this).css("color","black"); 
+		let contentId = $(this).attr("id");
+		location.href="http://localhost:8099/travelspot/post?contentId="+contentId//이동
 	})//click end
 	
-	
+	$(".placeLocation").click(function(){
+		//$(this).css("color","#2463d3"); 
+		let contentId = $(this).attr("id");
+		location.href="http://localhost:8099/travelspot/post?contentId="+contentId//이동
+		
+	})
 	
 	//선택했다가 뒤로 되돌아오면 글자 색상 원래대로, 테두리 원래대로 변경 추가
 	

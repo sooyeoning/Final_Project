bb4de981036e10340c9c2eea17d25768774692a6
HUN-E <HUN-E@DESKTOP-PAR813I>
2023-07-25 16:46:51 +0900

    homepage

--- kdt_final_project/src/main/java/controller/IndexController.java
+++ kdt_final_project/src/main/java/controller/IndexController.java
@@ -1,15 +1,31 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 	
+	@Autowired
+	HomeService service;
+	
 	@RequestMapping("/")
-	public String Index() {
+	public String Index(Model model) {
+		
+		 List<HomeDTO> getTop5Record = service.getTop5Record();
+		 model.addAttribute("getTop5Record",getTop5Record);
+		  
+		 List<HomeDTO> getTop5Recommend = service.getTop5Recommend();
+		 model.addAttribute("getTop5Recommend",getTop5Recommend);
+		 
 		return "index"; 
 	}
+	
+	
 	@RequestMapping("/index(Eng)")
 	public String EngIndex() {
 		return "index(Eng)"; 

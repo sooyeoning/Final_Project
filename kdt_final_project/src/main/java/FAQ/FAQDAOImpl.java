8c92095db47ba3c69a2057beca49846b7bebc2bd
DoHyunDaniel <132706505+DoHyunDaniel@users.noreply.github.com>
2023-08-02 10:41:16 +0900

    Update FAQDAOImpl.java

--- kdt_final_project/src/main/java/FAQ/FAQDAOImpl.java
+++ kdt_final_project/src/main/java/FAQ/FAQDAOImpl.java
@@ -1,6 +1,8 @@
 package FAQ;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +22,7 @@ public class FAQDAOImpl implements FAQDAO {
 	@Override
 	public void insertArticle(FAQDTO faqDTO) {
 		// TODO Auto-generated method stub
-		sqlSession.insert("FAQ.FAQDAO.insertArticle", faqDTO);
+		sqlSession.insert("insertArticle", faqDTO);
 
 	}
 
@@ -29,6 +31,15 @@ public class FAQDAOImpl implements FAQDAO {
 		return sqlSession.selectList("selectAllFAQs",writer); // SQL 매핑된 쿼리를 실행하여 Board 목록을 가져옴
 	}
 
+	@Override
+	public List<FAQDTO> selectAllFAQsForAdmin(String writer) {
+		return sqlSession.selectList("selectAllFAQsForAdmin",writer); // SQL 매핑된 쿼리를 실행하여 Board 목록을 가져옴
+	}
+	
+	@Override
+	public BoardDTO answer(int id) {
+		return sqlSession.selectOne("answer",id); // SQL 매핑된 쿼리를 실행하여 Board 목록을 가져옴
+	}
 
 	/*
 	 * @Override public String getWriterById(int id) { // 게시물 작성자 정보를 가져오는 SQL 쿼리를
@@ -37,10 +48,10 @@ public class FAQDAOImpl implements FAQDAO {
 	@Override
     public void deleteArticleById(int id) {
         // 게시물 삭제를 위한 SQL 쿼리를 실행
-        sqlSession.delete("FAQ.FAQDAO.deleteArticleById", id);
+        sqlSession.delete("deleteArticleById", id);
     }
 	
-	@Override
+    @Override
     public void updateFAQ(BoardDTO dto) {
         // 게시물 삭제를 위한 SQL 쿼리를 실행
         sqlSession.update("updateFAQ", dto);
@@ -49,7 +60,18 @@ public class FAQDAOImpl implements FAQDAO {
 	@Override
 	public BoardDTO getBoardById2(int id) {
 		// id에 따라 값을 받아와 board객체에 저장?
-		return sqlSession.selectOne("getBoardById",id);
+		return sqlSession.selectOne("getBoardById", id);
 	}
+	
+    public List<FAQDTO> selectFAQListForAdmin(int startIdx, int count) {
+        Map<String, Object> param = new HashMap<>();
+        param.put("startIdx", startIdx);
+        param.put("count", count);
+        return sqlSession.selectList("faq.selectFAQListForAdmin", param);
+    }
+
+    public int selectTotalFAQCount() {
+        return sqlSession.selectOne("faq.selectTotalFAQCount");
+    }
 
 }

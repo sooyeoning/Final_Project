93e1b93a929b5548f887a80d5a96ed657995ae29
HUN-E <HUN-E@DESKTOP-PAR813I>
2023-08-07 14:50:59 +0900

    테마 여행지 날씨 추가

--- kdt_final_project/src/main/resources/static/js/travelspot/travelspot_post_theme.js
+++ kdt_final_project/src/main/resources/static/js/travelspot/travelspot_post_theme.js
@@ -71,10 +71,64 @@ $(document).ready(function() {
 
 				var mapx = placeContentsDTO.mapx; //위도
 				var mapy = placeContentsDTO.mapy; //경도
+				
 				var title = placeContentsDTO.title;
-
+				var latitude = mapy;//위도
+    			var longitude = mapx;//경도
+   
+    			var apiURI = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+"487c6b16cacfbb98c9f1f5b9790f3b71";
+$.ajax({
+        		url: apiURI,
+        		dataType: "json",
+        		type: "GET",
+        		async: "false",
+        	success: function(resp) {
+        	 console.log("날씨 이미지 : "+ resp.weather[0].icon );
+        	var weatherIcon = {
+        		    '01' : 'fas fa-sun',
+        		    '02' : 'fas fa-cloud-sun',
+        		    '03' : 'fas fa-cloud',
+        		    '04' : 'fas fa-cloud-meatball',
+        		    '09' : 'fas fa-cloud-sun-rain',
+        		    '10' : 'fas fa-cloud-showers-heavy',
+        		    '11' : 'fas fa-poo-storm',
+        		    '13' : 'far fa-snowflake',
+        		    '50' : 'fas fa-smog'
+        		};
+            
+            var imgURL = "https://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
+            $("html컴포넌트").attr("src", imgURL);
+            var $Icon = (resp.weather[0].icon).substr(0,2);
+            var $Icon = (imgURL);
+            var $weather_description = resp.weather[0].main;
+            var $Temp = Math.floor(resp.main.temp- 273.15) + 'º';
+            var $humidity = '습도&nbsp;&nbsp;&nbsp;&nbsp;' + resp.main.humidity+ ' %';
+            var $wind = '바람&nbsp;&nbsp;&nbsp;&nbsp;' +resp.wind.speed + ' m/s';
+            var $city = '현위치&nbsp;&nbsp;'+resp.name;
+            var $cloud = '구름&nbsp;&nbsp;&nbsp;&nbsp;' + resp.clouds.all +"%";
+            var $temp_min = '최저 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_min- 273.15) + 'º';
+            var $temp_max = '최고 온도&nbsp;&nbsp;&nbsp;&nbsp;' + Math.floor(resp.main.temp_max- 273.15) + 'º';
+            
+			
+            $('.weather_icon').append('<i class="' + weatherIcon[$Icon] +' fa-5x" style="height : 150px; width : 150px;"></i>');
+            $('.weather_description').prepend($weather_description);
+            $('.current_temp').html($Temp);
+            $('.humidity').prepend($humidity);
+            $('.wind').prepend($wind);
+            $('.city').html($city);
+            $('.cloud').append($cloud);
+            $('.temp_min').append($temp_min);
+            $('.temp_max').append($temp_max);
+            
+            document.getElementById("imgsrc").src="https://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
+            document.getElementById("imgsrcd").src="https://openweathermap.org/img/w/" + resp.weather[0].icon + ".png";
+            	
+            
+        }
+   
+    });
 				//위도, 경도 이용해서 날씨 아이콘 띄우기
-				$('div[class="weather"]').html(mapx);
+				//$('div[class="weather"]').html(mapx);
 
 				//마커 표시
 				var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
